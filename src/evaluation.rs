f71f78d6b3709e8eb39898a9e17bb729a8eafca3
izraigo <izraigo@gmail.com>
2024-02-12 22:38:26 +0100

    Initial

---
+++ src/evaluation.rs
@@ -0,0 +1,139 @@
+use std::collections::HashMap;
+use crate::parser::LispVal;
+use crate::parser::LispVal::{Atom, Boolean, Number};
+
+#[derive(Clone, Debug, PartialEq, Default)]
+pub struct Env {
+    parent: Option<Box<Env>>,
+    vars: HashMap<String, LispVal>
+}
+
+impl Env {
+    pub fn new() -> Self {
+        Default::default()
+    }
+}
+
+pub fn eval(v: LispVal, mut env: &mut Box<Env>) -> Result<LispVal, String> {
+    match v {
+        LispVal::Atom(var) => get_var(var, env),
+        LispVal::String(_) => Ok(v),
+        LispVal::Number(_) => Ok(v),
+        LispVal::Boolean(_) => Ok(v),
+        LispVal::Quote(q) => Ok(*q),
+        LispVal::List(v) => eval_list(&v, &mut env),
+        LispVal::DottedList(_, _) => Ok(v),
+        LispVal::Func { .. } => todo!()
+    }
+}
+
+fn eval_list(list: &Vec<LispVal>, env: &mut Box<Env>) -> Result<LispVal, String> {
+    if let Ok(l) = apply_primitive(list, env) {
+        return Ok(l);
+    } else {
+        let a = &list[0];
+        if let Atom(s) = a {
+            match s.as_str() {
+                "define" => define_var(list, env),
+                "set!" => set_var(list, env),
+                _ => get_var(s.clone(), env)
+            }
+        } else {
+            return Err("".to_string())
+        }
+    }
+
+}
+
+fn consume(opt: Option<&LispVal>, e: &str) -> Result<LispVal, String> {
+    match opt {
+        Some(v) => Ok(v.clone()),
+        None => Err(e.to_string())
+    }
+}
+
+fn nothing_to_consume(opt: Option<&LispVal>) -> Result<(), String> {
+    match opt {
+        Some(v) => Err(format!("Error unexpected value {}", v)),
+        None => Ok(())
+    }
+}
+
+
+fn extract_str_from_atom(r: Result<LispVal, String>) -> Result<String, String> {
+    match r {
+        Ok(Atom(s)) => Ok(s),
+        Err(e) => Err(e),
+        Ok(other)=> Err(format!("Expected atom but got {}", other))
+    }
+}
+
+fn define_var(list: &Vec<LispVal>, env: &mut Box<Env>) -> Result<LispVal, String> {
+    let mut iter = list.iter();
+    consume(iter.next(), "Expect define")?;
+    let name = extract_str_from_atom(consume(iter.next(), "Expect variable name"))?;
+    let val = consume(iter.next(), "Expect variable value").map(|a| eval(a, env))??;
+    nothing_to_consume(iter.next())?;
+    env.vars.insert(name, val.clone());
+    Ok(val)
+}
+
+fn set_var(list: &Vec<LispVal>, env: &mut Box<Env>) -> Result<LispVal, String> {
+    let mut iter = list.iter();
+    consume(iter.next(), "Expect set!")?;
+    let name = extract_str_from_atom(consume(iter.next(), "Expect variable name"))?;
+    let val = consume(iter.next(), "Expect variable value").map(|a| eval(a, env))??;
+    nothing_to_consume(iter.next())?;
+    match env.vars.insert(name.clone(), val.clone()) {
+        Some(_) => Err(format!("Variable is not defined {}", name)),
+        None => Ok(val)
+    }
+}
+
+fn get_var(name: String, env: &mut Box<Env>) -> Result<LispVal, String> {
+    match env.vars.get(&name) {
+        Some(v) => Ok(v.clone()),
+        None => Err(format!("Variable {} is not defined", name))
+    }
+}
+
+fn apply_primitive(list: &Vec<LispVal>, env: &mut Box<Env> ) -> Result<LispVal, String> {
+    let mut iter = list.iter();
+    let operator = consume(iter.next(), "Expect operator")?;
+    let left = extract_num_value(consume(iter.next(), "Expect argument")?, env)?;
+    let right = extract_num_value(consume(iter.next(), "Expect argument")?, env)?;
+
+    if let Atom(s) = operator {
+        match s.as_str() {
+            "+" => Ok(Number(left + right)),
+            "-" => Ok(Number(left - right)),
+            "*" => Ok(Number(left * right)),
+            "/" => Ok(Number(left / right)),
+            "<" => Ok(Boolean(left < right)),
+            ">" => Ok(Boolean(left > right)),
+            "=" => Ok(Boolean(left == right)),
+            "!=" => Ok(Boolean(left != right)),
+            _ => Err(format!("Invalid infix operator: {}", s)),
+        }
+    } else {
+        Err(format!("Operation is not recognised: {}", operator))
+    }
+}
+
+fn extract_num_value(lv: LispVal, env: &mut Box<Env>) -> Result<i64, String> {
+    let left = eval(lv, env)?;
+    match left {
+        LispVal::Number(n) => Ok(n),
+        LispVal::String(s) => Ok(s.parse().unwrap()),
+        _ => return Err(format!("Left operand must be an integer {:?}", left)),
+    }
+}
+
+fn extract_string_value(lv: LispVal, env: &mut Box<Env>) -> Result<String, String> {
+    let left = eval(lv, env)?;
+    match left {
+        LispVal::Number(n) => Ok(n.to_string()),
+        LispVal::String(s) => Ok(s),
+        _ => return Err(format!("Left operand must be a string {:?}", left)),
+    }
+}
---
+++ src/parser.rs
@@ -0,0 +1,194 @@
+use std::fmt::Display;
+use nom::branch::alt;
+use nom::bytes::complete::{is_a, is_not};
+use nom::character::complete::{alpha1, alphanumeric1, char, digit1, space0, space1};
+use nom::IResult;
+use nom::multi::{many0, many1, separated_list1};
+use nom::sequence::{delimited, separated_pair};
+use crate::Env;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum LispVal {
+    Atom(String),
+    Number(i64),
+    String(String),
+    Boolean(bool),
+    List(Vec<LispVal>),
+    DottedList(Vec<LispVal>, Box<LispVal>),
+    Quote(Box<LispVal>),
+    Func { args:Vec<String>, vararg: Option<String>, body: Box<LispVal>, closure: Env}
+}
+
+impl Display for LispVal {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self {
+            LispVal::Atom(s) => write!(f, "{}", s),
+            LispVal::Number(n) => write!(f, "{}", n),
+            LispVal::String(s) => write!(f, "\"{}\"", s),
+            LispVal::Boolean(b) => write!(f, "{}", b),
+            LispVal::List(v) =>
+                {
+                    let a: Vec<String> = v.into_iter().map(|i| i.to_string()).collect();
+                    write!(f, "({})", a.join(" "))
+                },
+            LispVal::DottedList(v, v1) => {
+                let a: Vec<String> = v.into_iter().map(|i| i.to_string()).collect();
+                write!(f, "{} . {}", a.join(" "), v1.to_string())
+            }
+            LispVal::Quote(q) => write!(f, "quote {}", q),
+            LispVal:: Func { .. } => write!(f, "Func {}", "")
+        }
+    }
+}
+
+fn parse_string(input: &str) -> IResult<&str, LispVal> {
+    delimited(char('"'), is_not("\""), char('"'))(input)
+        .map(|( i, o)| (i, LispVal::String(String::from(o))))
+    //Ok((input, String::new()))
+}
+
+#[test]
+fn string_parser_test() {
+    let output = parse_string("\"hello\"").unwrap();
+    assert_eq!(output, ("", LispVal::String("hello".to_owned())) );
+}
+
+fn parse_number(input: &str) -> IResult<&str, LispVal> {
+    many1(digit1)(input)
+        .map(|( i, o) | (i, LispVal::Number(o.join("").parse::<i64>().unwrap())))
+}
+
+#[test]
+fn number_parser_test() {
+    assert!(parse_number("j5").is_err());
+    assert!(parse_number("jlsdf").is_err());
+    assert_eq!(parse_number("23").unwrap(), ("", LispVal::Number(23)));
+}
+
+fn match_symbols(input: String) -> LispVal {
+    match input.as_str() {
+        "#t" => LispVal::Boolean(true),
+        "#f" => LispVal::Boolean(false),
+        _ => LispVal::Atom(input),
+    }
+}
+
+/*fn symbol() -> impl Fn(&str) -> IResult<(&str, &str), &str>{
+    is_a("!#$%&|*+-/:<=>?@^_~")
+}
+*/
+fn parse_atom(input: &str) -> IResult<&str, LispVal> {
+    let (input, first) = alt((alpha1, symbol))(input)?;
+    let (input, rest) = many0(alt((alphanumeric1, symbol)))(input)?;
+    Ok((input, match_symbols(format!("{}{}", String::from(first), rest.join("")))))
+}
+
+fn symbol(s: &str) -> IResult<&str, &str> {
+    is_a("!#$%&|*+-/:<=>?@^_~")(s)
+}
+
+#[test]
+fn atom_parser_test() {
+    assert_eq!(
+        parse_atom("$foo").unwrap(),
+        ("", LispVal::Atom("$foo".to_owned()))
+    );
+    assert_eq!(parse_atom("#f").unwrap(), ("", LispVal::Boolean(false)));
+}
+
+fn parse_list(input: &str) -> IResult<&str, LispVal> {
+    separated_list1(space1, parse_expr)(input)
+        .map(|(i, v)| (i, LispVal::List(v)))
+}
+
+fn parse_quoted(input: &str) -> IResult<&str, LispVal> {
+    let (input, _) = char('\'')(input)?;
+    parse_expr(input)
+        .map(|(i, l )| (i, LispVal::Quote(Box::new(l))))
+}
+
+fn parse_quoted2(input: &str) -> IResult<&str, LispVal> {
+    let (input, _) = char('\'')(input)?;
+    parse_expr(input)
+        .map(|(i, l )| (i, LispVal::List(vec![LispVal::Atom("quote".to_owned()), l])))
+}
+
+pub fn parse_expr(input: &str) -> IResult<&str, LispVal> {
+    alt((parse_atom, parse_number, parse_string, parse_quoted, try_parse_list))(input)
+}
+
+fn dotted(input: &str) -> IResult<&str, &str> {
+    let (input, _) = space0(input)?;
+    let (input, _) = char('.')(input)?;
+    let (input, _) = space0(input)?;
+    Ok((input, "."))
+}
+
+fn parse_dotted_list(input: &str) -> IResult<&str, LispVal> {
+    separated_pair(parse_list, dotted, parse_expr)(input)
+        .map(|(i, (head, rest))|
+            {
+                let list = match head {
+                    LispVal::List(v) => v,
+                    _ => panic!("List parser returned a non-list value")
+                };
+                (i, LispVal::DottedList(list, Box::new(rest)))
+            }
+        )}
+
+fn try_parse_list(input: &str) -> IResult<&str, LispVal> {
+    let (input, _) = char('(')(input)?;
+    let (input, items) = alt((parse_dotted_list, parse_list))(input)?;
+    let (input, _) = char(')')(input)?;
+    Ok((input, items))
+}
+
+#[test]
+fn list_parser_test() {
+    assert_eq!(
+        parse_list("$foo 42 53").unwrap(),
+        (
+            "",
+            LispVal::List(vec!(
+                LispVal::Atom("$foo".to_owned()),
+                LispVal::Number(42),
+                LispVal::Number(53)
+            ))
+        )
+    );
+    assert_eq!(
+        parse_list("\"foo\" 42 53").unwrap(),
+        (
+            "",
+            LispVal::List(vec!(
+                LispVal::String("foo".to_owned()),
+                LispVal::Number(42),
+                LispVal::Number(53)
+            ))
+        )
+    );
+}
+
+#[test]
+fn quoted_parser_test() {
+    let output = parse_quoted("'52").unwrap();
+    assert_eq!(
+        output,
+        (
+            "",
+            LispVal::Quote(Box::new(LispVal::Number(52)))
+        )
+    );
+}
+
+#[test]
+fn quoted_parser_test2() {
+    let output = parse_quoted2("'52").unwrap();
+    assert_eq!(
+        output,
+        (
+            "",
+            LispVal::List(vec![LispVal::Atom("quote".to_owned()), LispVal::Number(52)])
+        )
+    );
+}
